b2a2abde3846414c14cbf5e151e69906d5b494bc
Wlad Paiva <desenvolvedormg@gmail.com>
2022-10-24 18:49:55 -0300

    add docs_url link

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,7 +4,12 @@ export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      This comes from a third package:
       <Button />
+
+      <h2>access other projects</h2>
+      This PR introduces a way to access the other projects PR
+      <a href={process.env.DOCS_URL}>Read the docs</a>
     </div>
   );
 }
